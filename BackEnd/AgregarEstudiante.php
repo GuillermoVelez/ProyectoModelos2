9b6743ee6feb5ddc14cd85386f72241636ac6f67
GuillermoVelez <memocol40@gmail.com>
2021-02-19 22:14:12 -0500

    Agregar Estudiante

---
+++ BackEnd/AgregarEstudiante.php
@@ -0,0 +1,25 @@
+<?php
+//$conex =mysql_connect("localhost","root","","tutorial");
+$NombreRecibido=$_POST['Nombre'];
+$IdentifiacionRecibido=$_POST['Identifiacion'];
+$ApellidoRecibido=$_POST['Apellido'];
+$EdadRecibido=$_POST['Edad'];
+$DireccionRecibido=$_POST['Direccion'];
+$conex = new mysqli("localhost","root","","tutorial");
+
+
+/*if($conex->connect_error){
+	die("Connection failed: " . $conn->connect_error);
+	echo "<h2> No nos conectamos<h2>";
+}else{
+	echo "<h2> Si nos conectamos<h2>";
+}*/
+$sql= "INSERT INTO alumnos(idAlumno,Nombre,Apellido,Edad,Direccion_Residencia) VALUES ('$IdentifiacionRecibido','$NombreRecibido','$ApellidoRecibido','$EdadRecibido','$DireccionRecibido')";
+if ($conex->query($sql)=== TRUE){
+	header("index.html");
+}else{
+	//echo "No se pudo identificar: " . $sql . "<br>" . $conex->error;
+}
+$conex->close();
+?>
+
